9bd42da14440d2c5078058b2853b5d2725286b37
vishnuacappella <acappella.studiov@gmail.com>
2022-07-04 17:05:31 +0530

    constructor completed

--- bin/class_object.dart
+++ bin/class_object.dart
@@ -1,12 +1,28 @@
 // !class
+// class Person {
+//   String? name;
+//   int age = 25;
+// }
+
+// void main(List<String> arguments) {
+// !object declaration
+//   final person = Person();
+//   person.name = 'Vishnu';
+//   print(person.name);
+// }
+
+// !class
 class Person {
-  String? name;
-  int age = 25;
+  final String name;
+  final int age;
+
+  // !Constructor
+  Person(this.name, this.age);
 }
 
 void main(List<String> arguments) {
   // !object declaration
-  final person = Person();
-  person.name = 'Vishnu';
+  final person = Person('Vishnu2', 25);
+  // person.name = 'Vishnu';
   print(person.name);
 }
